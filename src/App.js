089043f0f16472c359fa1b87006a61c4c106f514
Matheus Ricci <57235913+MR1CC1@users.noreply.github.com>
2023-05-24 16:48:21 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
 import { useState } from 'react';
 import { useFetch } from './hooks/useFetch';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import ChevronRightOutlinedIcon from '@mui/icons-material/ChevronRightOutlined';
 
 const url = "http://localhost:3000/produtos"
 
@@ -8,19 +17,16 @@ function App() {
 
   const [nome, setNome] = useState("");
   const [preco, setPreco] = useState("");
-
-  const {data: itens, httpConfig, loading, error} = useFetch(url)
+  const { data: itens, httpConfig, loading, error } = useFetch(url)
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     const produto = {
       nome,
       preco,
     }
 
     httpConfig(produto, "POST")
-
     setNome("");
     setPreco("");
   }
@@ -31,30 +37,34 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Lista de Produtos</h1>
+      <p><a style={{ textDecoration: 'underline', color: 'grey', fontSize: '2rem' }} href="https://www.linkedin.com/in/matheus-ricci-228a06182/">Linkedin Matheus Ricci</a></p>
+      <h1 style={{ color: 'grey', fontSize: '2rem' }}>Lista de Itens</h1>
       {loading && <p>Carregando Dados...</p>}
       {error && <p>{error}</p>}
       {!error && (
-      <ul>
-        {itens && itens.map((produto) => (
-          <li key={produto.id}>
-            {produto.nome} - R$: {produto.preco}
-            <button onClick={() => handleRemove(produto.id)}>Excluir</button>
-          </li>
-        ))}
-      </ul>)}
+        <ul>
+          {itens && itens.map((produto) => (
+            <li key={produto.id}>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <ChevronRightOutlinedIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={`${produto.nome} - R$: ${produto.preco}`} />
+                <IconButton edge="end" onClick={() => handleRemove(produto.id)} aria-label="delete">
+                  <DeleteIcon />
+                </IconButton>
+              </ListItem>
+            </li>
+          ))}
+        </ul>)}
       <div className='add-produto'>
         <form onSubmit={handleSubmit}>
-          <label>
-            Nome:
-            <input type='text' value={nome} onChange={(e) => setNome(e.target.value)}/>
-          </label>
-          <label>
-            Preço:
-            <input type='text' value={preco} onChange={(e) => setPreco(e.target.value)}/>
-          </label>
-          {loading && <input type='submit' disabled value="Aguarde"></input>}
-          {!loading && <input type='submit' value="Criar"></input>}
+          <p><TextField type='text' value={nome} id="standard-basic" onChange={(e) => setNome(e.target.value)} label="Item" variant="standard" /></p>
+          <p><TextField type='text ' value={preco} id="standard-basic" onChange={(e) => setPreco(e.target.value)} label="Preço" variant="standard" /></p>
+          {loading && <Button style={{ marginTop: '30px' }} type='submit' disabled >Aguarde</Button>}
+          {!loading && <Button style={{ marginTop: '30px' }} type='submit' variant="outlined">Adicionar</Button>}
         </form>
       </div>
     </div>
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -11,28 +11,28 @@ export const useFetch = (url) => {
     const [itemId, setItemId] = useState(null)
 
     const httpConfig = async (data, method) => {
-        if(method === "POST"){
-           setConfig({
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data),
-           })
+        if (method === "POST") {
+            setConfig({
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data),
+            })
 
-           setMethod(method)
-        } else if( method === "DELETE"){
+            setMethod(method)
+        } else if (method === "DELETE") {
             setConfig({
                 method: "DELETE",
                 headers: {
-                  "Content-Type": "application/json"
+                    "Content-Type": "application/json"
                 },
-               })
-               setMethod("DELETE")
-               setItemId(data)
+            })
+            setMethod("DELETE")
+            setItemId(data)
         }
     }
-    
+
     useEffect(() => {
         const fetchData = async () => {
             setLoading(true)
@@ -40,7 +40,7 @@ export const useFetch = (url) => {
                 const res = await fetch(url);
                 const json = await res.json();
                 setData(json)
-            }catch(error){
+            } catch (error) {
                 setError("Houve um Erro ao carregar dados!")
             }
             setLoading(false)
@@ -51,16 +51,17 @@ export const useFetch = (url) => {
     useEffect(() => {
         const httpRequest = async () => {
             let json;
-            if(method === "POST"){
+            if (method === "POST") {
                 let fetchOptions = [url, config]
                 const res = await fetch(...fetchOptions)
                 json = await res.json();
                 setCallFetch(json)
-            } else if(method === "DELETE"){
+            } else if (method === "DELETE") {
                 const deleteUrl = `${url}/${itemId}`
                 const res = await fetch(deleteUrl, config)
                 json = await res.json();
-            } 
+                setCallFetch(json)
+            }
         }
         httpRequest();
     }, [config, method, url, itemId])
